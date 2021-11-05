33a8076a6598f7ea311e1ced598b9429f75a82c5
Constanza <cxnstanzagamarra@gmail.com>
2021-11-04 22:54:59 -0300

    Primer commit - form reactive sin estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ControlContainer, FormBuilder, FormGroup, MinLengthValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form';
+  title = 'reactive-form';
+ 
+  public formLogin:FormGroup;
+
+  constructor(private formBuilder:FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.formLogin=this.formBuilder.group( {
+      email: [' ' ,
+        [ Validators.required ,
+          Validators.email
+        ]
+      ],
+      password:['',
+        [
+         Validators.required ,
+         Validators.minLength(6)
+        ]
+      ],
+      terms: ['',
+        [
+          Validators.required , 
+          Validators.requiredTrue
+        ]
+      ]
+    
+    });
+
+    this.loadAPI
+  }
+
+  loadAPI():any {
+    const response= {
+      email: 'prueba@gmail.com' ,
+      password: '1234567', 
+      terms:true
+    };
+
+    this.formLogin.patchValue(response);
+
+  }
+
+  send():any {
+    console.log(this.formLogin.value);
+  }
 }
